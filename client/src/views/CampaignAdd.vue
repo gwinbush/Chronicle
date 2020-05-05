9bc7673d6b450a4ab879d6cf6c0891084a0cc722
Gabriel Winbush <gabrielwinbush@Gabriels-Air.fios-router.home>
2020-05-04 20:59:01 -0400

    Added create test and view test functionality

--- client/src/views/CampaignAdd.vue
+++ client/src/views/CampaignAdd.vue
@@ -8,16 +8,15 @@
         <div class="column" style='background-color:#cbf1f5'>
             <form>
                 <v-text-field
-                    v-model="name"
+                    v-model="subject1"
                     :error-messages="nameErrors"
-                    :counter="10"
                     label="Subject"
                     required
                     @input="$v.name.$touch()"
                     @blur="$v.name.$touch()"
                 ></v-text-field>
                 <v-textarea
-                    v-model="email"
+                    v-model="message1"
                     :error-messages="emailErrors"
                     label="Message Body"
                     required
@@ -31,7 +30,7 @@
         <div class="column" style='background-color:#ffaaa6'>
             <form>
                 <v-text-field
-                    v-model="name"
+                    v-model="subject2"
                     :error-messages="nameErrors"
                     :counter="10"
                     label="Subject"
@@ -40,7 +39,7 @@
                     @blur="$v.name.$touch()"
                 ></v-text-field>
                 <v-textarea
-                    v-model="email"
+                    v-model="message2"
                     :error-messages="emailErrors"
                     label="Message Body"
                     required
@@ -50,25 +49,58 @@
                     @blur="$v.email.$touch()"
                 ></v-textarea>
                 <v-select
-                    v-model="select"
-                    :items="items"
+                    v-model="selected_cohort"
+                    :items="Object.keys(cohorts)"
                     :error-messages="selectErrors"
                     label="Cohort feature"
                     required
                     @change="$v.select.$touch()"
                     @blur="$v.select.$touch()"
+                    v-on:change="updateSelectedCohort"
                 ></v-select>
                 <v-select
-                    v-model="select"
-                    :items="items"
+                    v-model="selected_value"
+                    :items="selected_cohort_values"
                     :error-messages="selectErrors"
                     label="Cohort value"
                     required
                     @change="$v.select.$touch()"
                     @blur="$v.select.$touch()"
+                    no-data-text="Please select cohort feature"
                 ></v-select>
-            <v-btn class="mr-4" @click="submit">submit</v-btn>
+            <v-btn class="mr-4" @click="submitTest">submit</v-btn>
             </form>
+        
+        <v-dialog
+        v-model="dialog"
+        width="500"
+        >
+        <v-card>
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            A/B Test Created
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            Your test has been created.<br> The ID is: {{campaign_id}}. <br> Go to the "View A/B Tests" page to see the results.
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
             
         </div>
     </v-content>
@@ -78,7 +110,7 @@
 
 <script>
 import axios from "axios";
-import Navbar from '@/components/Navbar.vue'
+import Navbar from '@/components/Navbar.vue';
 export default {
     name: 'CampaignsView',
     components: {
@@ -86,27 +118,69 @@ export default {
     },
     data () {
         return {
-            campaigns: []
+            subject1:"",
+            message1: "",
+            subject2:"",
+            message2:"",
+            cohorts: {
+                "age": [ 21, 22, 23 ],
+                "city": [
+                    "Pittsburgh",
+                    "Ithaca",
+                    "McLean"
+                ]
+            },
+            selected_cohort:"",
+            selected_cohort_values:[],
+            selected_value:"",
+            dialog: false,
+            campaign_id:""
         }
     },
     mounted() {
-        axios({ method: "GET", "url": "https://drtk2lbaij.execute-api.us-east-1.amazonaws.com/api/v1/campaigns/list" }).then(result => {
-                this.campaigns = result.data;
-            }, error => {
-                console.error(error);
-            });
+
     },
     methods: {
-        submit () {
-            this.$v.$touch()
-            },
+
         clear () {
-            this.$v.$reset()
-            this.name = ''
-            this.email = ''
-            this.select = null
-            this.checkbox = false
+            this.subject1 = ''
+            this.subject2 = ''
+            this.message1 = ''
+            this.message2 = ''
+            this.selected_cohort = ''
+            this.selected_value = ''
+
         },
+        updateSelectedCohort(value) {
+            
+            console.log(value)
+            console.log(this.cohorts[value])
+            this.selected_cohort_values = this.cohorts[value]
+        },
+        submitTest() {
+            this.dialog = true
+            var body = {
+                "distribution_channel": "EMAIL",
+                "cohort_name": this.selected_cohort,
+                "cohort_value": this.selected_value,
+                "subject1": this.subject1,
+                "message1": this.message1,
+                "subject2": this.subject2,
+                "message2": this.message2
+            }
+            console.log(JSON.stringify(body))
+            var self = this
+            axios.post('https://drtk2lbaij.execute-api.us-east-1.amazonaws.com/api/v1/campaign', JSON.stringify(body))
+            .then(function (response) {
+                console.log(response['data']['CampaignResponse']['Id']);
+                self.campaign_id = response['data']['CampaignResponse']['Id'].toString()
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            this.clear()
+        }
+
     }
 }
 </script>
--- client/src/views/CampaignsView.vue
+++ client/src/views/CampaignsView.vue
@@ -2,17 +2,109 @@
 <v-app>
     <div>
         <Navbar/>
-        <h1>Campaigns View Page</h1>
     </div>
+
+    <v-content>
+        <v-container fluid>
+          <h1 style='padding-top:20px; padding-bottom:20px; text-align:center'>View A/B Test Results</h1>
+          <v-row justify="center">
+          <v-col class="d-flex" cols="12" sm="6" center>
+          <v-select
+            :items="campaigns"
+            label="Test ID"
+            solo
+            v-on:change="updateMetrics"
+          ></v-select>
+        </v-col>
+        </v-row>
+        <v-row>     
+        <v-col>   
+            <v-card
+                class="mx-auto"
+                max-width="344"
+                >
+                <v-card-text>
+                    <div></div>
+                    <p class="display-1 text--primary">Test A</p>
+                    <div class="text--primary"><b>Subject</b></div>
+                    <p>{{metrics["0"]["subject"]}}</p>
+                    <div class="text--primary"><b>Body</b></div>
+                    <p>{{metrics["0"]["body"]}}</p>
+                    <v-row>
+                        <v-col><v-icon>mdi-email-check</v-icon><p>{{metrics["0"]["emails_sent"]}} deliveries</p></v-col>
+                        <v-col><v-icon>mdi-email-open</v-icon><p>{{metrics["0"]["emails_opened"]}} opens</p></v-col>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col>
+            <v-card
+                class="mx-auto"
+                max-width="344"
+                >
+                <v-card-text>
+                    <div></div>
+                    <p class="display-1 text--primary">Test A</p>
+                    <div class="text--primary"><b>Subject</b></div>
+                    <p>{{metrics["1"]["subject"]}}</p>
+                    <div class="text--primary"><b>Body</b></div>
+                    <p>{{metrics["1"]["body"]}}</p>
+                    <v-row>
+                        <v-col><v-icon>mdi-email-check</v-icon><p>{{metrics["1"]["emails_sent"]}} deliveries</p></v-col>
+                        <v-col><v-icon>mdi-email-open</v-icon><p>{{metrics["1"]["emails_opened"]}} opens</p></v-col>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        </v-row>
+        
+        </v-container>
+    </v-content>
 </v-app>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
+import axios from "axios";
+import Navbar from '@/components/Navbar.vue';
 export default {
     name: 'CampaignsAdd',
     components: {
         Navbar
+    },
+    data () {
+        return {
+            campaigns: [],
+            metrics: {
+                "0":{
+                    "subject": "",
+                    "body": "",
+                    "emails_sent": 0,
+                    "emails_opened": 0
+                },
+                "1":{
+                    "subject": "",
+                    "body": "",
+                    "emails_sent": 0,
+                    "emails_opened": 0
+                }
+            }
+        }
+    },
+    mounted() {
+        axios({ method: "GET", "url": "https://drtk2lbaij.execute-api.us-east-1.amazonaws.com/api/v1/campaigns/list" }).then(result => {
+                this.campaigns = result.data['campaign_ids'];
+            }, error => {
+                console.error(error);
+            });
+    },
+    methods: {
+        updateMetrics(campaign_id) {
+            axios({ method: "GET", "url": `https://drtk2lbaij.execute-api.us-east-1.amazonaws.com/api/v1/metrics?campaign_id=${campaign_id}`}).then(result => {
+                this.metrics = result.data;
+            }, error => {
+                console.error(error);
+            });
+        }
     }
 }
 </script>
